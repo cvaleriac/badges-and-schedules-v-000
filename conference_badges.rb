daabebd1e73e8cb60e111d2e3a7d9832960073cb
valeria campilongo <cvaleriac@hotmail.it>
2018-04-11 20:53:26 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -7,3 +7,10 @@ attendees.collect do |name|
   badge_maker(name)
     end
   end
+  def assign_rooms(attendees)
+  room=0
+   attendees.collect do |name|
+  room+=1
+   "Hello, #{name}! You'll be assigned to room #{room}!"
+   end
+end
