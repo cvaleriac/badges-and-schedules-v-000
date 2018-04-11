2b785ef5d1adbde581a5481fa315692be29ae2a6
valeria campilongo <cvaleriac@hotmail.it>
2018-04-11 20:36:14 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,10 @@
-# Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+attendees.collect do |name|
+  badge_maker(name)
+    end
+  end
